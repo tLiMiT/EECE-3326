54c4db220d00dad5bfa6b7b7a84b3a3b5ecff624
Tim <tim.liming@gmail.com>
2012-10-26 17:35:34 -0400

    Project 4a

--- limyue-4a/limyue-4a/p4.cpp
+++ limyue-4a/limyue-4a/p4.cpp
@@ -44,36 +44,58 @@ public:
 	ValueType getCell(int, int);
 
 	// Added functions
-	//void printConflicts();
-	//bool conflicts();
+	void printConflicts();
+	bool conflicts(int i, int j, int val);
 	void setCell(int i, int j, int val);
-	//void clearCell();
-	//bool solvedBoard();
+	void clearCell(int i, int j);
+	bool solvedBoard();
 private:
 	// Added function
-	//void updateConflicts();
+	void updateConflicts(int i, int j, int num, bool val);
 
 	// The following matrices go from 1 to BoardSize in each
 	// dimension.  ie. they are each (BoardSize+1)x(BoardSize+1)
 	matrix<ValueType> value;
+
+	matrix<bool> rows;		// matrix of each row conflict
+	matrix<bool> cols;		// matrix of each column conflict
+	matrix<bool> sqrs;		// matrix of each square conflict
 };
 
 board::board(int sqSize)
-	: value(BoardSize+1,BoardSize+1)
+	: value(BoardSize+1, BoardSize+1),
+	  rows(BoardSize+1, BoardSize+1),
+	  cols(BoardSize+1, BoardSize+1),
+	  sqrs(BoardSize+1, BoardSize+1)
 // Board constructor
 {
 }
 
+void board::clearCell(int i, int j)
+// clear the sudoku board
+{
+	updateConflicts(i, j, value[i][j], 0);
+	value[i][j] = 0;
+} // clearCell
+
 void board::clear()
 {
-}
+	for (int i = 1; i < value.rows(); i++)
+	{
+		for (int j = 1; j < value.cols(); j++)
+		{
+			clearCell(i, j);
+		} // for
+	} // for
+} // clear
 
 void board::initialize(ifstream &fin)
 // Read a Sudoku board from the input file.
 {
 	char ch;
 
-	clear();
+	clear();	// clear incase there is a board already loaded
+
 	for (int i = 1; i <= BoardSize; i++)
 	{
 		for (int j = 1; j <= BoardSize; j++)
@@ -83,8 +105,16 @@ void board::initialize(ifstream &fin)
 			// If the read char is not Blank
 			if (ch != '.')
 			{
-				setCell(i, j, atoi(&ch));   // Convert char to int
-			} // if
+				if (conflicts(i, j, ch-'0'))
+				{
+					throw rangeError("conflict on initialization");
+				}
+				setCell(i, j, ch-'0');	// Convert char to int
+			} 
+			else if (ch == '.')
+			{
+				setCell(i, j, 0);		// set cell to 0
+			}// if - else if
 		} // for
 	} // for
 } // initialize
@@ -98,6 +128,20 @@ int squareNumber(int i, int j)
 
 	return SquareSize * ((i-1)/SquareSize) + (j-1)/SquareSize + 1;
 }
+
+bool board::conflicts(int i, int j, int val)
+// check the row, column, and square for conflicts of a given value
+{
+	return rows[i][val] || cols[j][val] || sqrs[squareNumber(i, j)][val];
+} // conflicts
+
+void board::updateConflicts(int i, int j, int num, bool val)
+// update each of the three possible conflict areas
+{
+	rows[i][num] = val;
+	cols[j][num] = val;
+	sqrs[squareNumber(i, j)][num] = val;
+} // updateConflicts
 
 ostream &operator<<(ostream &ostr, vector<int> &v)
 	// Overloaded output operator for vector class.
@@ -128,6 +172,7 @@ void board::setCell(int i, int j, int val)
 // set the value of the cell
 {
 	value[i][j] = val;
+	updateConflicts(i, j, val, true);
 } // setCell
 
 bool board::isBlank(int i, int j)
@@ -137,7 +182,29 @@ bool board::isBlank(int i, int j)
 	{
 		throw rangeError("bad value in setCell");
 	}
+	else
+	{
+		return value[i][j] == 0;
+	}
 } // isBlank
+
+bool board::solvedBoard()
+// check if the sudoku board is solved
+{
+	for (int i = 1; i < value.rows(); i++)
+	{
+		for (int j = 1; j < value.cols(); j++)
+		{
+			if (value[i][j] == 0 || conflicts(i, j, value[i][j]))
+			{
+				return false;
+			} // if
+		} // for
+	} // for
+	cout << endl;
+	print();			// print the solved board
+	return true;
+} // solvedBoard
 
 void board::print()
 // Prints the current board.
@@ -165,7 +232,7 @@ void board::print()
 			{
 				cout << " " << getCell(i, j) << " ";
 			}
-			else
+			else if (isBlank(i, j))
 			{
 				cout << "   ";
 			}
@@ -182,6 +249,38 @@ void board::print()
 	cout << "-";
 	cout << endl;
 } // print
+
+
+void board::printConflicts()
+// print each of the conflict matrices side by side
+{
+	// prints titles spaced correctly
+	cout << "Row Conflicts        ";
+	cout << "Column Conflicts      ";
+	cout << "Square Conflicts" << endl;
+
+	for (int i = 1; i < value.rows(); i++)
+	{
+		for (int j = 1; j < value.cols(); j++)
+		{
+			// print a row matrix row
+			cout << rows[i][j] << ' ';
+		}
+		cout << "   ";		// add space between each matrix
+		for (int j = 1; j < value.cols(); j++)
+		{
+			// print a column matrix row
+			cout << cols[i][j] << ' ';
+		}
+		cout << "    ";		// add space between each matrix
+		for (int j = 1; j < value.cols(); j++)
+		{
+			// print a square matrix row
+			cout << sqrs[i][j] << ' ';
+		}
+		cout << endl;		// move to the next line
+	} // for
+} // printConflicts
 
 int main()
 {
@@ -205,7 +304,15 @@ int main()
 		{
 			b1.initialize(fin);
 			b1.print();
-			//b1.printConflicts();
+			b1.printConflicts();
+			if (b1.solvedBoard())
+			{
+				cout << "The board is solved.\n" << endl;
+			}
+			else if (! b1.solvedBoard())
+			{
+				cout << "The board is not solved.\n" << endl;
+			}
 		}
 	}
 	catch  (indexRangeError &ex)
@@ -213,5 +320,5 @@ int main()
 		cout << ex.what() << endl;
 		exit(1);
 	}
-}
+} // main
 
